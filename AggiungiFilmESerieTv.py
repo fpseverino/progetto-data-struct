8efad68132b50e07c049cabdc6107f0108023695
GabrieleTrovato01 <trovato.gabriele10@gmail.com>
2024-05-13 13:03:02 +0200

    Implementazione Mappa

---
+++ AggiungiFilmESerieTv.py
@@ -0,0 +1,65 @@
+from adt import UnsortedTableMap
+import Contenuti
+
+def salva_film(k,v):
+    file = open("film.txt","a")
+    file.write(f"Titolo:{k}")
+    file.write("\n")
+    file.write(f"Genere:{v.genere}")
+    file.write("\n")
+    file.write(f"Durata:{v.durata}")
+    file.write("\n")
+    file.write(f"Regista:{v.regista}")
+    file.write("\n")
+    file.close()
+
+def salva_serie_tv(k, v):
+    file = open("serietv.txt", "a")
+    file.write(f"Titolo:{k}")
+    file.write("\n")
+    file.write(f"Genere:{v.genere}")
+    file.write("\n")
+    file.write(f"Durata:{v.durata}")
+    file.write("\n")
+    file.write(f"Regista:{v.regista}")
+    file.write("\n")
+    file.write(f"Numero episodi:{v.num_episodi}")
+    file.write("\n")
+    file.write(f"Numero stagioni:{v.num_stagioni}")
+    file.write("\n")
+    file.close()
+
+map = UnsortedTableMap.UnsortedTableMap()
+
+while True:
+    print("Cosa vuoi salvare?\n1. Film\n2. Serie Tv")
+    scelta = int(input())
+    if scelta == 1:
+
+        titolo = input("Titolo del film\n")
+
+        print("Inserisci il genere")
+        genere = input()
+        print("Inserisci la durata")
+        durata = input()
+        print("Inserisci il regista")
+        regista = input()
+        contenutofilm = Contenuti.ContenutoFilm(genere, durata, regista)
+        map.__setitem__(titolo, contenutofilm)
+        salva_film(titolo, contenutofilm)
+
+    else:
+        titolo = input("Titolo Serie Tv\n")
+        print("Inserisci il genere")
+        genere = input()
+        print("Inserisci la durata media di un episodio")
+        durata = input()
+        print("Inserisci il regista")
+        regista = input()
+        print("Inserisci il numero di episodi")
+        num_episodi = input()
+        print("Inserisci il numero di stagioni")
+        num_stagioni = input()
+        contenutoserie = Contenuti.ContenutoSerieTv(genere, durata, regista, num_episodi, num_stagioni)
+        map.__setitem__(titolo, contenutoserie)
+        salva_serie_tv(titolo, contenutoserie)
--- main.py
+++ main.py
@@ -1,3 +1,52 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+from adt import UnsortedTableMap
+import Contenuti
+
+if __name__ == "__main__":
+    mappa = UnsortedTableMap.UnsortedTableMap()
+    file = open("film.txt", "r")
+    for line in file:
+        if line.startswith("Titolo:"):
+            titolo = line.split(":")[1]
+            titolo = titolo.split("\n")[0]
+        elif line.startswith("Genere:"):
+            genere = line.split(":")[1]
+            genere = genere.split("\n")[0]
+        elif line.startswith("Durata:"):
+            durata = line.split(":")[1]
+            durata = durata.split("\n")[0]
+        elif line.startswith("Regista:"):
+            regista = line.split(":")[1]
+            regista = regista.split("\n")[0]
+            contenutifilm = Contenuti.ContenutoFilm(genere, durata, regista)
+            mappa.__setitem__(titolo, contenutifilm)
+
+    file.close()
+    file = open("serietv.txt", "r")
+    for line in file:
+        if line.startswith("Titolo:"):
+            titolo = line.split(":")[1]
+            titolo = titolo.split("\n")[0]
+        elif line.startswith("Genere:"):
+            genere = line.split(":")[1]
+            genere = genere.split("\n")[0]
+        elif line.startswith("Durata:"):
+            durata = line.split(":")[1]
+            durata = durata.split("\n")[0]
+        elif line.startswith("Regista:"):
+            regista = line.split(":")[1]
+            regista = regista.split("\n")[0]
+        elif line.startswith("Numero episodi:"):
+            num_episodi = line.split(":")[1]
+            num_episodi = num_episodi.split("\n")[0]
+        elif line.startswith("Numero stagioni:"):
+            num_stagioni = line.split(":")[1]
+            num_stagioni = num_stagioni.split("\n")[0]
+            contenutoserie = Contenuti.ContenutoSerieTv(genere, durata, regista, num_episodi, num_stagioni)
+            mappa.__setitem__(titolo, contenutoserie)
+    file.close()
+
+    for k in mappa:
+        print(k)
+        print(mappa[k].genere)
+        print(mappa[k].durata)
+        print(mappa[k].regista)
